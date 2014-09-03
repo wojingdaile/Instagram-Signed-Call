fa6754c07ee9eab996ce4bfae0dd3549811815d0
yafengtang <tangyafeng1990@gmail.com>
2014-09-03 14:51:29 +0800

    remove chinese characters.

--- midds/igsigned/index.js
+++ midds/igsigned/index.js
@@ -8,13 +8,12 @@ var crypto = require('crypto');
 module.exports = function igsigned(req, res, next) {
     var ips = req.ips.join(',');
     var app_name = req.body.app_name || req.query.app_name || '';
-    var access_token = req.body.access_token || req.query.access_token || '';
 
     var app_config = config.Instagram[app_name];
     if (!app_config) {
         var err = new Error('PARAM_INVALID_ERROR');
         throw err;
-        console.error('[SignCheck]Invalid App Name:', app_name);
+        console.error('[SignCheck] Invalid App Name:', app_name);
         return;
     }
 
